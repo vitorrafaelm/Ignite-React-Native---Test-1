a28f2073ff67fd3c5b0798fc56e1712f460a1b3e
vitorrafaelm <vitor.rafael1518@gmail.com>
2022-06-25 12:11:46 -0300

    feat: task done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,30 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getMilliseconds(),
+      title: newTaskTitle, 
+      done: false,
+    }
+
+    setTasks(prev => [...prev, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const tasksUpdated = tasks.map(task => {
+      if(task.id === id){
+        task.done = !task.done; 
+      }
+
+      return task; 
+    });
+
+    setTasks(tasksUpdated);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksKeeped = tasks.filter(task => task.id !== id); 
+    setTasks(tasksKeeped); 
   }
 
   return (
